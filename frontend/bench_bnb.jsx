90a4bd48da809219cc111b6068b51f651a26c2e3
Dallas Morgan <dallaswmorgan@gmail.com>
2017-01-08 01:13:47 -0800

    Setup frontend auth framework

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+import { login, logout, signup } from './actions/session_actions';
+// import { login, logout } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
+  const store = configureStore();
+  window.store = store;
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
