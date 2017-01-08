3c7a7cafa1b74410e5f45a635977a2796bd68900
Dallas Morgan <dallaswmorgan@gmail.com>
2017-01-08 04:51:40 -0800

    Frontend auth completed

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -9,7 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
   window.login = login;
   window.logout = logout;
   window.signup = signup;
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+  }else {
+    store = configureStore();
+  }
   window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
